c5331be04156463d2340c55cbfe8a17f6c2da827
randyluna222 <108312411+randyluna222@users.noreply.github.com>
2024-01-20 11:10:22 -0600

    complete app

---
+++ app/about/page.jsx
@@ -0,0 +1,23 @@
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Consequatur quia veritatis, debitis enim tempore tempora quisquam 
+        aliquam natus deserunt atque labore quasi molestiae inventore d
+        ignissimos iusto pariatur amet sit sunt sapiente, 
+        placeat ipsam aut reprehenderit? Ullam sunt 
+        asperiores ducimus id architecto dignissimos 
+        et consequuntur accusantium, rem laudantium voluptate 
+        officia ipsum, reprehenderit dolores aut. Nam porro ipsa excepturi temporibus libero 
+        consequuntur vel adipisci enim doloribus rerum fugiat veniam, suscipit expedita.
+         Laudantium molestias obcaecati nemo libero totam, eaque corrupti a ducimus
+          voluptate atque deleniti nisi, ut qui maiores culpa adipisci dignissimos 
+          pariatur magni quidem porro autem sunt. 
+        Expedita dicta pariatur distinctio architecto.</p>
+      </div>
+  )
+  
+}
+
+export default AboutPage;
---
+++ app/services/page.jsx
@@ -0,0 +1,17 @@
+function ServicesPage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         In commodi cumque excepturi vel? Voluptatibus cupiditate 
+         fugiat dolor possimus perferendis consequuntur velit blanditiis
+          nemo ipsum. Totam iure earum mollitia error delectus tempore iusto
+           inventore sequi, est sapiente eveniet quas aperiam provident autem 
+           sunt rem reiciendis!
+         Consectetur error illum dolorum labore exercitationem!</p>
+      </div>
+  )
+  
+}
+
+export default ServicesPage;
